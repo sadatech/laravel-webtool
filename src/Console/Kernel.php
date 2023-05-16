2fa5d218334c7ae06cd56832838fa711eb8c4d06
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-05-16 11:05:27 +0700

    Update Kernel.php

--- src/Console/Kernel.php
+++ src/Console/Kernel.php
@@ -10,17 +10,7 @@ trait Kernel
      *
      * @var array
      */
-    protected $commands = [
+    public $commands = [
         \Sadatech\Webtool\Console\Commands\Console::class
     ];
-
-    /**
-     * Register the Closure based commands for the application.
-     *
-     * @return void
-     */
-    protected function commands()
-    {
-        require base_path('routes/console.php');
-    }
 }
