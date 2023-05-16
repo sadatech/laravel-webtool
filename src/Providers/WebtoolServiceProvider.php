8e547df9d0e8f82116157751315aae23ae33d120
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-05-16 09:51:11 +0700

    dev-master

--- src/Providers/WebtoolServiceProvider.php
+++ src/Providers/WebtoolServiceProvider.php
@@ -2,14 +2,27 @@
 namespace Sadatech\Webtool\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Route;
 
 class WebtoolServiceProvider extends ServiceProvider
 {
+    /**
+     * This namespace is applied to your controller routes.
+     *
+     * In addition, it is set as the URL generator's root namespace.
+     *
+     * @var string
+     */
+    protected $namespace = 'App\Http\Controllers';
+
+    /**
+     * Define your route model bindings, pattern filters, etc.
+     *
+     * @return void
+     */
     public function boot()
     {
-        $this->app->routeMiddleware([
-            'access' => ServiceAccessMiddleware::class,
-        ]);
+        parent::boot();
     }
     
 }
