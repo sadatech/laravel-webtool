5e0786dbfaea7382b60eb82ae447848e35b7cd2c
sadatech <github@sada.id>
2023-08-16 14:37:25 +0700

    test call

--- src/Console/Commands/Command.php
+++ src/Console/Commands/Command.php
@@ -47,7 +47,7 @@ class Command extends IlluminateCommand
         
         if ($type == "worker")
         {
-            Artisan::call("queue:work", ["--once", "--tries" => 1, "--timeout" => 1200, "--memory" => 4096, "--memory" => 3, "--sleep" => "3", "-vvv"]);
+            Artisan::call("queue:work", ["--once" => null, "--tries" => 1, "--timeout" => 1200, "--memory" => 4096, "--memory" => 3, "--sleep" => "3", "-vvv" => null]);
             $this->line("-- Webtool Worker Runner --");
         }
         else
