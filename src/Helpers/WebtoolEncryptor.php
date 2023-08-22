50530af2bb06a1bd7e6f135f0709ba7ed47c2869
sadatech <github@sada.id>
2023-08-22 16:31:17 +0700

    update encryptor

--- src/Helpers/WebtoolEncryptor.php
+++ src/Helpers/WebtoolEncryptor.php
@@ -2,4 +2,14 @@
 namespace Sadatech\Webtool\Helpers;
 
 class WebtoolEncryptor
-{}
+{
+    public function Make($string, $salt = '')
+    {
+        return "___::".$string;
+    }
+
+    public function Disassemble($keyName, $removeLink = false)
+    {
+        return "___::".$keyName;
+    }
+}
