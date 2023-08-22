3a44863bd61b94bc87ff140b490fd83acba9d083
sadatech <github@sada.id>
2023-08-22 16:44:50 +0700

    update package

--- src/Helpers/WebtoolEncryptor.php
+++ src/Helpers/WebtoolEncryptor.php
@@ -3,13 +3,40 @@ namespace Sadatech\Webtool\Helpers;
 
 class WebtoolEncryptor
 {
+    private $local_file_path;
+
+    public function __construct()
+    {
+        $this->local_file_path = sys_get_temp_dir().DIRECTORY_SEPARATOR;
+    }
+
     public function Make($string, $salt = '')
     {
-        return "___::".$string;
+        $keyName = hash('sha256', md5(sha1(base64_encode($string))).md5($salt));
+        $keyData = gzcompress($string, 9);
+
+        if (!file_exists($this->local_file_path.$keyName))
+        {
+            file_put_contents($this->local_file_path.$keyName, $keyData);
+        }
+
+        return $keyName;
     }
 
     public function Disassemble($keyName, $removeLink = false)
     {
-        return "___::".$keyName;
+        if (file_exists($this->local_file_path.$keyName))
+        {
+            $keyData = file_get_contents($this->local_file_path.$keyName);
+            $keyData = gzuncompress($keyData);
+
+            if ($removeLink)
+            {
+                unlink($this->local_file_path.$keyName);
+            }
+            return $keyData;
+        }
+        
+        return false;
     }
 }
