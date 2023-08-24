8d1deabd03870ca8195a0a1f319a272193c5269a
sadatech <github@sada.id>
2023-08-24 14:21:42 +0700

    update controller

--- src/Http/Controllers/WebtoolController.php
+++ src/Http/Controllers/WebtoolController.php
@@ -26,10 +26,12 @@ class WebtoolController extends Controller
         $process .= WebtoolHelper::DoCommand(['git', '-C', "/data/".request()->getHost()."/sadata-reporting", "status"]);
         $process .= WebtoolHelper::DoCommand(['git', '-C', "/data/".request()->getHost()."/sadata-reporting", "restore", "/data/".request()->getHost()."/sadata-reporting"]);
         $process .= WebtoolHelper::DoCommand(['git', '-C', "/data/".request()->getHost()."/sadata-reporting", "pull", "origin", "master"]);
-        $process .= WebtoolHelper::DoCommand(['php',"/data/".request()->getHost()."/sadata-reporting/artisan", "clear-compiled", "-vvv"]);
-        $process .= WebtoolHelper::DoCommand(['php',"/data/".request()->getHost()."/sadata-reporting/artisan", "cache:clear", "--no-interaction", "-vvv"]);
-        $process .= WebtoolHelper::DoCommand(['php',"/data/".request()->getHost()."/sadata-reporting/artisan", "view:clear", "--no-interaction", "-vvv"]);
-        $process .= WebtoolHelper::DoCommand(['php',"/data/".request()->getHost()."/sadata-reporting/artisan", "migrate", "--no-interaction", "--force", "-vvv"]);
+        $process .= WebtoolHelper::DoCommand(['php', "/data/".request()->getHost()."/sadata-reporting/artisan", "clear-compiled", "-vvv"]);
+        $process .= WebtoolHelper::DoCommand(['php', "/data/".request()->getHost()."/sadata-reporting/artisan", "cache:clear", "--no-interaction", "-vvv"]);
+        $process .= WebtoolHelper::DoCommand(['php', "/data/".request()->getHost()."/sadata-reporting/artisan", "view:clear", "--no-interaction", "-vvv"]);
+        $process .= WebtoolHelper::DoCommand(['php', "/data/".request()->getHost()."/sadata-reporting/artisan", "migrate", "--no-interaction", "--force", "-vvv"]);
+        $process .= WebtoolHelper::DoCommand(['php', "composer", "--working-dir=/data/".request()->getHost()."/sadata-reporting", "-n", "update", "--ignore-platform-req=ext-mongodb", "--no-plugins", "--no-interaction"]);
+        $process .= WebtoolHelper::DoCommand(['php', "composer", "--working-dir=/data/".request()->getHost()."/sadata-reporting", "-n", "dumpa", "--no-plugins", "-o", "-a"]);
         $process .= "</code></pre>";
 
         return $process;
