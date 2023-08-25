19c40eaa10ee9229a8f1d5ea8bbccc888cf9c12d
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-08-25 17:32:55 +0700

    update helper with generator url

--- src/Helpers/Webtool.php
+++ src/Helpers/Webtool.php
@@ -3,6 +3,7 @@ namespace Sadatech\Webtool\Helpers;
 
 use Symfony\Component\Process\Process;
 use Symfony\Component\Process\Exception\ProcessFailedException;
+use Sadatech\Webtool\Helpers\WebtoolEncryptor;
 
 class Webtool
 {
@@ -27,5 +28,43 @@ class Webtool
     public static function GetConfig($key, $value = null)
     {
         return config($key, $value);
+    }
+
+    public static function GenerateActionLink($item, $path)
+    {
+        $action['url']  = str_replace('http://localhost/', asset(''), $item->results);
+        $action['path'] = is_null($path) ? $item->results : ("/export/report/". $path . basename(str_replace('---123---', 'https', str_replace('https','---123---', str_replace('http','---123---', $action['url'])))));
+        $action['url']  = (new WebtoolEncryptor)->Make(json_encode(['id' => $item->id, 'location' => $action['path']]));
+        $action['html'] = '';
+
+        if (File::exists(public_path($action['path'])))
+        {
+            $action['html'] .= "
+            <form method='post' action='".route('export-download-get-temp')."?reqid=".hash('sha256', $action['url'].time())."'><input type='hidden' name='_token' value='".csrf_token()."'><input type='hidden' name='temp' value='".$action['url']."'>
+                <button type='button' style='width: 80%;' class='btn btn-sm btn-success btn-square disabled' disabled ><i class='fa fa-spinner fa-spin'></i></button>
+            </form>
+            ";
+        }
+        else
+        {
+            if ($item->url)
+            {
+                $action['html'] .= "
+                <form method='post' action='".route('export-download-get-temp')."?reqid=".hash('sha256', $action['url'].time())."'><input type='hidden' name='_token' value='".csrf_token()."'><input type='hidden' name='temp' value='".$action['url']."'>
+                    <button type='submit' formtarget='_blank' class='btn btn-sm btn-success btn-square'><i class='fa fa-cloud-download' style='width: 80%;'></i></button>
+                </form>
+                ";
+            }
+            else
+            {
+                $action['html'] .= "
+                <form method='post' action='".route('export-download-get-temp')."?reqid=".hash('sha256', $action['url'].time())."'><input type='hidden' name='_token' value='".csrf_token()."'><input type='hidden' name='temp' value='".$action['url']."'>
+                    <button type='button' class='btn btn-sm btn-success btn-square' style='width: 80%;'><i class='fa fa-spinner fa-spin'></i></button>
+                </form>
+                ";
+            }
+        }
+
+        return $action['html'];
     }
 }
