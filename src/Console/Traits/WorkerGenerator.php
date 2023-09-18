b7dd75e552361cd4f8c0db7292fe8e3ba8e90884
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-09-18 13:49:17 +0700

    update logger

--- src/Console/Traits/WorkerGenerator.php
+++ src/Console/Traits/WorkerGenerator.php
@@ -15,7 +15,7 @@ trait WorkerGenerator
         $this->line("Generating worker...");
 
         $_[] = $this->call("queue:work", ["--once" => null, "--tries" => Common::GetEnv('WORKER_TRIES', 3), "--timeout" => Common::GetEnv('WORKER_TIMEOUT', 1200), "--memory" => Common::GetEnv('WORKER_MEMORY', 4096), "--delay" => Common::GetEnv('WORKER_DELAY', 3), "--sleep" => Common::GetEnv('WORKER_SLEEP', 3), "--no-ansi" => null, "--no-interaction" => null, "-vvv" => null]);
-        Log::info(json_encode($_));
+        \Log::info(json_encode($_));
         // $this->call("webtool:fetch", ["--type" => "export-sync-files", "-vvv" => null]);
     }
 }
