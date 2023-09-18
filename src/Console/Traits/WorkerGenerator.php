24e28f1139da22aa4d83dde376f3fe6f59dad250
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-09-18 14:34:13 +0700

    update return message

--- src/Console/Traits/WorkerGenerator.php
+++ src/Console/Traits/WorkerGenerator.php
@@ -16,15 +16,10 @@ trait WorkerGenerator
      */
     public function WebtoolDoWorker()
     {
-        $this->line("");
-        $this->line("Generating worker...");
-
         $_[] = $this->call("queue:work", ["--once" => null, "--tries" => Common::GetEnv('WORKER_TRIES', 3), "--timeout" => Common::GetEnv('WORKER_TIMEOUT', 1200), "--memory" => Common::GetEnv('WORKER_MEMORY', 4096), "--delay" => Common::GetEnv('WORKER_DELAY', 3), "--sleep" => Common::GetEnv('WORKER_SLEEP', 3), "--no-ansi" => null, "--no-interaction" => null, "-vvv" => null]);
         $_[] = $this->WebtoolValidateSyncFiles();
         $_[] = $this->WebtoolDoExportSyncFiles();
         $_[] = shell_exec('$(which sleep) 15');
-
-        $this->line("");
     }
 
     /**
