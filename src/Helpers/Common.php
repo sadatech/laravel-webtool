02c1d1d1a14fc4866acd855af917aaf16b16fe71
Andika Muhammad Cahya <andikamc@users.noreply.github.com>
2023-11-01 09:57:51 +0700

    Create new function helper `curl_get_contents`

--- src/Helpers/Common.php
+++ src/Helpers/Common.php
@@ -1,6 +1,7 @@
 <?php
 namespace Sadatech\Webtool\Helpers;
 
+use Exception;
 use Illuminate\Support\Facades\File;
 use Sadatech\Webtool\Helpers\Encryptor;
 
@@ -66,5 +67,22 @@ class Common
                 $i = -1;
             }
         }
+    }
+
+    public static function curl_get_contents($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        $data = curl_exec($ch);
+        if (curl_errno($ch))
+        {
+            throw new Exception('Failed to execute CURL operations.');
+        }
+        curl_close($ch);
+
+        return $data;
     }
 }
