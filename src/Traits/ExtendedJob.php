90cea5bc7d13763488fd15cba7db5cd1ca8cb238
Andika Muhammad Cahya <andkmc99@gmail.com>
2023-11-02 12:32:02 +0700

    Update `ExtendedJob` with closure process data

--- src/Traits/ExtendedJob.php
+++ src/Traits/ExtendedJob.php
@@ -6,15 +6,38 @@ use GuzzleHttp\Client as HTTP_Client;
 
 trait ExtendedJob
 {
-    public function MakeRequestNode($scheme, $endpoint, $build_query)
+    public function MakeRequestNode($scheme, $endpoint, $build_query, $job_trace = NULL)
     {
         $http_scheme = strtoupper($scheme);
-        $http_endpoint = 'https://dataproc.sadata.id/'.$endpoint;
+        $http_endpoint = Common::GetEnv('DATAPROC_URL', 'https://dataproc.sadata.id/').$endpoint;
         $http_build_query = $build_query;
         $http_client = new HTTP_Client;
 
         $http_response = $http_client->request($http_scheme, $http_endpoint, ['form_params' => $http_build_query]);
 
-        return $http_response;
+        if (!is_null($job_trace))
+        {
+            if ($http_response->getStatusCode() == 200)
+            {
+                $http_response = json_decode($http_response->getBody()->getContents());
+                $result        = @$http_response->path;
+
+                $job_trace->update([
+                    'log' => @$http_response->hash,
+                ]);
+            }
+            else
+            {
+                $job_trace->update([
+                    'status' => 'FAILED',
+                    'log'    => 'Failed to export data (from node)',
+                ]);
+            }
+        }
+        else
+        {
+            return $http_response;
+        }
+
     }
 }
