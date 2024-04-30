0c0b819c7d09ec1173006f274f4262ad39ecd435
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-05-01 02:19:29 +0700

    Fix URL parsing in WorkerHelper.php

--- src/Helpers/WorkerHelper.php
+++ src/Helpers/WorkerHelper.php
@@ -8,22 +8,12 @@ class WorkerHelper
     public static function ValidateResultBaseURL($job_trace, $parse_url, $base_url)
     {
         if (isset($parse_url['scheme']))
+        {
+            if (CommonHelper::GetEnv('DATAPROC_URL') == '' || is_null(CommonHelper::GetEnv('DATAPROC_URL')))
             {
-                if (CommonHelper::GetEnv('DATAPROC_URL') == '' || is_null(CommonHelper::GetEnv('DATAPROC_URL')))
-                {
-                    if ($parse_url['host'] !== @parse_url(CommonHelper::GetEnv('DATAPROC_URL', 'https://dataproc.sadata.id/'))['host'])
-                    {
-                        return str_replace($parse_url['host'], request()->getHost(), $job_trace->results);
-                    }
-                }
-                else
-                {
-                    return $base_url;
-                }
+                if ($parse_url['host'] !== @parse_url(CommonHelper::GetEnv('DATAPROC_URL', 'https://dataproc.sadata.id/'))['host']) return str_replace($parse_url['host'], request()->getHost(), $job_trace->results);
             }
-            else
-            {
-                return $base_url;
-            }
+        }
+        return $base_url;
     }
 }
