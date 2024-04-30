f09d75669f152ad2d0724d34d808bd1a6518cf8f
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-04-30 12:05:22 +0700

    Refactor HealthcheckController.php to update response message and add WebtoolPackage namespace

--- src/Http/Controllers/HealthcheckController.php
+++ src/Http/Controllers/HealthcheckController.php
@@ -2,6 +2,7 @@
 namespace Sadatech\Webtool\Http\Controllers;
 
 use Sadatech\Webtool\Http\Controllers\Controller;
+use Sadatech\Webtool\Package as WebtoolPackage;
 
 class HealthcheckController extends Controller
 {
@@ -10,7 +11,8 @@ class HealthcheckController extends Controller
         return response()->json([
             'status' => 'OK',
             'code' => 200,
-            'message' => 'Webtool is running'
+            'message' => ucfirst(WebtoolPackage::PACKAGE_NAME) . ' is running',
+            'namespace' => WebtoolPackage::PACKAGE_NAMESPACE,
         ]);
     }
 }
