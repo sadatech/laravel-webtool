4a7f5019cb5e1b4c742ae30a204ed1af0510c668
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-05-01 02:18:12 +0700

    Refactor WorkerHelper.php to fix URL parsing and use CommonHelper for environment variable access

--- src/Helpers/WorkerHelper.php
+++ src/Helpers/WorkerHelper.php
@@ -1,15 +1,17 @@
 <?php
 namespace Sadatech\Webtool\Helpers;
 
+use Sadatech\Webtool\Helpers\CommonHelper;
+
 class WorkerHelper
 {
     public static function ValidateResultBaseURL($job_trace, $parse_url, $base_url)
     {
         if (isset($parse_url['scheme']))
             {
-                if (Common::GetEnv('DATAPROC_URL') == '' || is_null(Common::GetEnv('DATAPROC_URL')))
+                if (CommonHelper::GetEnv('DATAPROC_URL') == '' || is_null(CommonHelper::GetEnv('DATAPROC_URL')))
                 {
-                    if ($parse_url['host'] !== @parse_url(Common::GetEnv('DATAPROC_URL', 'https://dataproc.sadata.id/'))['host'])
+                    if ($parse_url['host'] !== @parse_url(CommonHelper::GetEnv('DATAPROC_URL', 'https://dataproc.sadata.id/'))['host'])
                     {
                         $stream_base_url = str_replace($parse_url['host'], request()->getHost(), $job_trace->results);
                     }
