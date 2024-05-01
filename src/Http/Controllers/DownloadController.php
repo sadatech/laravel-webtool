6d693fa2a7d2f360354763f02485ed02fadc3173
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-05-01 20:35:02 +0700

    Refactor DownloadController.php to include buffer for storing data

--- src/Http/Controllers/DownloadController.php
+++ src/Http/Controllers/DownloadController.php
@@ -3,11 +3,17 @@ namespace Sadatech\Webtool\Http\Controllers;
 
 use Illuminate\Support\Facades\Artisan;
 use Sadatech\Webtool\Http\Controllers\Controller;
+use Sadatech\Webtool\Helpers\EncryptorHelper;
 
 class DownloadController extends Controller
 {
-    public function GeneralDownloadCloud()
+    private $buffer = [];
+
+    public function GeneralDownloadCloud($uid)
     {
-        return 'ahai';
+        $this->buffer['uid'] = $uid;
+        $this->buffer['pkg'] = json_decode((new EncryptorHelper)->Disassemble($this->buffer['uid']));
+
+        dd($this->buffer);
     }
 }
