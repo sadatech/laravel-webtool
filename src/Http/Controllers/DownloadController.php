c373195c071d53cbf6f84b7353119a0332e6b7ae
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-05-01 21:00:29 +0700

    Refactor DownloadController.php to fix URL parsing and optimize file handling

--- src/Http/Controllers/DownloadController.php
+++ src/Http/Controllers/DownloadController.php
@@ -2,6 +2,7 @@
 namespace Sadatech\Webtool\Http\Controllers;
 
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Storage;
 use Sadatech\Webtool\Http\Controllers\Controller;
 use Sadatech\Webtool\Helpers\EncryptorHelper;
 use Sadatech\Webtool\Helpers\CommonHelper;
@@ -19,10 +20,26 @@ class DownloadController extends Controller
         if (isset($this->buffer['pkg']->id))
         {
             $this->buffer['job_trace'] = JobTrace::where('id', $this->buffer['pkg']->id)->first();
-            $this->buffer['file_path'] = str_replace(CommonHelper::GetConfig('filesystems.disks.spaces.url'), null, urldecode($this->buffer['job_trace']->url));
-            $this->buffer['file_path'] = explode('/', $this->buffer['file_path']);
+            $this->buffer['file_path'] = explode('/', str_replace(CommonHelper::GetConfig('filesystems.disks.spaces.url'), null, urldecode($this->buffer['job_trace']->url)));
             array_shift($this->buffer['file_path']);
             $this->buffer['file_path'] = trim(implode('/', $this->buffer['file_path']));
+
+            if (empty($this->buffer['job_trace']))
+            {
+                if (Storage::disk('spaces')->exists($this->buffer['file_path']))
+                {
+                    $this->buffer['download_cloud_name'] = basename($this->buffer['file_path']);
+                    $this->buffer['download_cloud_size'] = Storage::disk('spaces')->size($this->buffer['file_path']);
+                    $this->buffer['download_cloud_mime'] = Storage::disk('spaces')->mimeType($this->buffer['file_path']);
+                    $this->buffer['download_cloud_url']  = str_replace("https://sadata-cdn.sgp1.digitaloceanspaces.com", CommonHelper::GetConfig('filesystems.disks.spaces.url'), Storage::disk('spaces')->url($this->buffer['file_path']))
+
+                    try
+                    {
+                        $this->buffer['download_global_url']  = str_rot13(base64_encode(Storage::disk('spaces')->url($this->buffer['file_path'])));
+                        $this->buffer['download_global_path'] = CommonHelper::FetchGetContent('https://global-mirror.sadata.id', true, false, ['url' => $this->buffer['download_global_url']]);
+                    }
+                }
+            }
         }
 
         dd($this->buffer);
