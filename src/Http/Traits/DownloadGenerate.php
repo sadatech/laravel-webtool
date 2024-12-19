db6efa854ebc05e6b281b214566c8fce1a1c39d2
Andika Muhammad Cahya <andkmc99@gmail.com>
2024-12-19 13:39:22 +0700

    Add validation to handle non-zip file downloads and improve expiration logic

--- src/Http/Traits/DownloadGenerate.php
+++ src/Http/Traits/DownloadGenerate.php
@@ -28,89 +28,47 @@ trait DownloadGenerate
             array_shift($download['path']);
             $download['path'] = trim(implode('/', $download['path']));
             $download['path'] = str_replace('//', '/', $download['path']);
-    
-            // validate url not empty
-            if (!empty($download['trace']->url))
-            {
-                // set validate expired time
-                $download['time_start'] = Carbon::parse($download['trace']->created_at)->addDays(5)->timestamp;
-                $download['time_end']   = Carbon::now()->timestamp;
-    
-                // validate expired time
-                if ($download['time_start'] < $download['time_end'])
+
+            if (pathinfo($download['trace']->url, PATHINFO_EXTENSION) !== 'zip') {
+                // validate url not empty
+                if (!empty($download['trace']->url))
                 {
-                    if (FileStorage::disk("spaces")->exists($download['path']))
+                    // set validate expired time
+                    $download['time_start'] = Carbon::parse($download['trace']->created_at)->addDays(5)->timestamp;
+                    $download['time_end']   = Carbon::now()->timestamp;
+        
+                    // validate expired time
+                    if ($download['time_start'] < $download['time_end'])
                     {
-                        // delete from spaces
-                        FileStorage::disk("spaces")->delete($download['path']);
-    
-                        // update tracejob
-                        $download['trace']->update([
-                            'status'      => 'DELETED',
-                            'url'         => NULL,
-                            'results'     => NULL,
-                            'other_notes' => 'File has expired.',
-                            'log'         => 'File may no longer be available due file has expired.',
-                        ]);        
-                    }
-                }
-                else
-                {
-                    // download approved
-                    if (FileStorage::disk("spaces")->exists($download['path']))
-                    {
-                        $download['s3name'] = basename($download['path']);
-                        $download['s3size'] = FileStorage::disk("spaces")->size($download['path']);
-                        $download['s3mime'] = FileStorage::disk("spaces")->mimeType($download['path']);
-    
-                        $send_global_url  = FileStorage::disk("spaces")->url($download['path']);
-                        $send_global_url  = base64_encode($send_global_url);
-                        $send_global_url  = str_rot13($send_global_url);
-                        // $cloud_url_real   = str_replace("https://sadata-cdn.sgp1.digitaloceanspaces.com", Common::GetConfig('filesystems.disks.spaces.url'), FileStorage::disk("spaces")->url($download['path']));
-                        $cloud_url_real   = str_replace('https://'.Common::GetConfig('filesystems.disks.spaces.bucket').str_replace('https://', '.', Common::GetConfig('filesystems.disks.spaces.endpoint')), Common::GetConfig('filesystems.disks.spaces.url'), FileStorage::disk("spaces")->url($download['path']));
-
-                        try
+                        if (FileStorage::disk("spaces")->exists($download['path']))
                         {
-                            $send_global_data = Common::FetchGetContent(Common::GetEnv('MIRROR_URL', "https://dl-trx-cf.sadata.id"), true, false, ["url" => $send_global_url]);
-
-                            if ($send_global_data['http_code'] !== 200)
-                            {
-                                $scheme_cloud_url = parse_url($cloud_url_real);
-                                if (isset($scheme_cloud_url['scheme']))
-                                {
-                                    return redirect()->to($cloud_url_real);
-                                }
-
-                                return response()->json($send_global_data);
-                            }
-                            else
-                            {
-                                $send_data = json_decode($send_global_data['data']);
-
-                                if (isset($send_data->data->preview_url))
-                                {
-                                    return redirect()->to($send_data->data->preview_url);
-                                }
-                                else
-                                {
-                                    return redirect()->to($cloud_url_real);
-                                }
-                            }
-                        }
-                        catch (Exception $ex)
-                        {
-                            return redirect()->to($cloud_url_real);
+                            // delete from spaces
+                            FileStorage::disk("spaces")->delete($download['path']);
+        
+                            // update tracejob
+                            $download['trace']->update([
+                                'status'      => 'DELETED',
+                                'url'         => NULL,
+                                'results'     => NULL,
+                                'other_notes' => 'File has expired.',
+                                'log'         => 'File may no longer be available due file has expired.',
+                            ]);        
                         }
                     }
                     else
                     {
-                        if (!empty($download['trace']->results))
+                        // download approved
+                        if (FileStorage::disk("spaces")->exists($download['path']))
                         {
-                            // return redirect()->to($download['trace']->results);
-                            $send_global_url  = FileStorage::disk("spaces")->url($download['trace']->results);
+                            $download['s3name'] = basename($download['path']);
+                            $download['s3size'] = FileStorage::disk("spaces")->size($download['path']);
+                            $download['s3mime'] = FileStorage::disk("spaces")->mimeType($download['path']);
+        
+                            $send_global_url  = FileStorage::disk("spaces")->url($download['path']);
                             $send_global_url  = base64_encode($send_global_url);
                             $send_global_url  = str_rot13($send_global_url);
-                            $cloud_url_real   = $download['trace']->results;
+                            // $cloud_url_real   = str_replace("https://sadata-cdn.sgp1.digitaloceanspaces.com", Common::GetConfig('filesystems.disks.spaces.url'), FileStorage::disk("spaces")->url($download['path']));
+                            $cloud_url_real   = str_replace('https://'.Common::GetConfig('filesystems.disks.spaces.bucket').str_replace('https://', '.', Common::GetConfig('filesystems.disks.spaces.endpoint')), Common::GetConfig('filesystems.disks.spaces.url'), FileStorage::disk("spaces")->url($download['path']));
 
                             try
                             {
@@ -118,7 +76,13 @@ trait DownloadGenerate
 
                                 if ($send_global_data['http_code'] !== 200)
                                 {
-                                    return redirect()->to($cloud_url_real);
+                                    $scheme_cloud_url = parse_url($cloud_url_real);
+                                    if (isset($scheme_cloud_url['scheme']))
+                                    {
+                                        return redirect()->to($cloud_url_real);
+                                    }
+
+                                    return response()->json($send_global_data);
                                 }
                                 else
                                 {
@@ -141,13 +105,55 @@ trait DownloadGenerate
                         }
                         else
                         {
-                            $download['trace']->update([
-                                'status' => 'DELETED',
-                                'log' => 'File may no longer be available due to an export error or the file has expired.',
-                            ]);
+                            if (!empty($download['trace']->results))
+                            {
+                                // return redirect()->to($download['trace']->results);
+                                $send_global_url  = FileStorage::disk("spaces")->url($download['trace']->results);
+                                $send_global_url  = base64_encode($send_global_url);
+                                $send_global_url  = str_rot13($send_global_url);
+                                $cloud_url_real   = $download['trace']->results;
+
+                                try
+                                {
+                                    $send_global_data = Common::FetchGetContent(Common::GetEnv('MIRROR_URL', "https://dl-trx-cf.sadata.id"), true, false, ["url" => $send_global_url]);
+
+                                    if ($send_global_data['http_code'] !== 200)
+                                    {
+                                        return redirect()->to($cloud_url_real);
+                                    }
+                                    else
+                                    {
+                                        $send_data = json_decode($send_global_data['data']);
+
+                                        if (isset($send_data->data->preview_url))
+                                        {
+                                            return redirect()->to($send_data->data->preview_url);
+                                        }
+                                        else
+                                        {
+                                            return redirect()->to($cloud_url_real);
+                                        }
+                                    }
+                                }
+                                catch (Exception $ex)
+                                {
+                                    return redirect()->to($cloud_url_real);
+                                }
+                            }
+                            else
+                            {
+                                $download['trace']->update([
+                                    'status' => 'DELETED',
+                                    'log' => 'File may no longer be available due to an export error or the file has expired.',
+                                ]);
+                            }
                         }
                     }
                 }
+            }
+            else
+            {
+                return redirect()->to($download['trace']->url);
             }
         }
         else
